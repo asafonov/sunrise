a0b3bcdb16f885799b1b55c54365e64ab9763caa
asafonov <me@asafonov.org>
2024-01-02 10:42:36 +0300

    initial commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// play() changes:
+// for class .icon_play - .icon_on to .icon_off
+// for class .icon_pause - .icon_off to .icon_on
+
+// pause() makes the opposite:
+// for class .icon_play - .icon_off to .icon_on
+// for class .icon_pause - .icon_on to .icon_off
