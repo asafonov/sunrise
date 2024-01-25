75629da0ff63fba24944f17d2d2a2764eb87ec77
asafonov <me@asafonov.org>
2024-01-25 11:17:06 +0300

    ugly play

--- controllers/drumtrack.js
+++ controllers/drumtrack.js
@@ -3,15 +3,18 @@ class DrumTrackController {
   constructor (name, tempo, length) {
     this.model = new DrumTrack(name, length)
     this.tempo = tempo || 120
+    this.interval = 60 / this.tempo * 1000
     this.addEventListeners()
   }
 
   addEventListeners() {
     asafonov.messageBus.subscribe(asafonov.events.TRACK_VIEW_UPDATED, this, 'onTrackViewUpdate')
+    asafonov.messageBus.subscribe(asafonov.events.IS_PLAYING_UPDATED, this, 'onIsPlayingUpdate')
   }
 
   removeEventListeners() {
     asafonov.messageBus.unsubscribe(asafonov.events.TRACK_VIEW_UPDATED, this, 'onTrackViewUpdate')
+    asafonov.messageBus.unsubscribe(asafonov.events.IS_PLAYING_UPDATED, this, 'onIsPlayingUpdate')
   }
 
   play() {
@@ -29,6 +32,18 @@ class DrumTrackController {
     asafonov.messageBus.send(asafonov.events.TRACK_MODEL_UPDATED, data)
   }
 
+  onIsPlayingUpdate ({isPlaying}) {
+    this.timeout && clearTimeout(this.timeout)
+
+    if (isPlaying) {
+      const track = this.model.getTrack()
+
+      for (let i = 0; i < track.length; ++i) {
+        if (track[i]) setTimeout(() => this.play(), i * this.interval)
+      }
+    }
+  }
+
   getModel() {
     return this.model
   }
@@ -36,6 +51,9 @@ class DrumTrackController {
   destroy() {
     this.removeEventListeners()
     this.tempo = null
+    this.timeout && clearTimeout(this.timeout)
+    this.timeout = null
+    this.interval = null
     this.model.destroy()
     this.model = null
   }
