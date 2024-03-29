dd59eeb211dfd621544574f35ba9c1821525671d
asafonov <me@asafonov.org>
2024-03-29 15:37:34 +0000

    track view

--- view/drumtracklist.js
+++ view/drumtracklist.js
@@ -2,12 +2,21 @@ class DrumTrackListView {
 
   constructor (list) {
     this.container = document.querySelector('.drumtrack')
-    this.container.style.display = 'none'
     this.views = []
 
     for (let i = 0; i < list.length; ++i) {
       this.views.push(new DrumTrackView(list[i], asafonov.colors[i % asafonov.colors.length], this.container))
     }
+
+    this.hide()
+  }
+
+  hide() {
+    this.container.style.display = 'none'
+  }
+
+  show() {
+    this.container.style.display = 'flex'
   }
 
   destroy() {
--- view/track.js
+++ view/track.js
@@ -5,35 +5,65 @@ class TrackView {
     this.instruments = {}
 
     if (data.drums) {
-      this.drums = {
+      this.instruments.drums = {
         view: new DrumTrackListView(data.drums),
         container: this.container.querySelector('.drums')
       }
-      this.drums.container.style.display = 'flex'
+      this.instruments.drums.container.style.display = 'flex'
     }
 
     if (data.instruments) {
       for (let i = 0; i < data.instruments.length; ++i) {
         this.instruments[data.instruments[i]] = {
-          view: new InstrumentTrackView(data.instruments[i])),
+          view: new InstrumentTrackView(data.instruments[i]),
           container: this.container.querySelector(`.${data.instruments[i]}`)
         }
         this.instruments[data.instruments[i]].container.style.display = 'flex'
       }
     }
+
+    this.onTrackClickProxy = this.onTrackClick.bind(this)
+    this.addEventListeners()
+  }
+
+  addEventListeners() {
+    for (let i in this.instruments) {
+      this.instruments[i].container.addEventListener('click', this.onTrackClickProxy)
+    }
+  }
+
+  removeEventListeners() {
+    for (let i in this.instruments) {
+      this.instruments[i].container.removeEventListener('click', this.onTrackClickProxy)
+    }
+  }
+
+  hide() {
+    for (let i in this.instruments) {
+      this.instruments[i].container.style.display = 'none'
+    }
+  }
+
+  show() {
+    for (let i in this.instruments) {
+      this.instrument[i].container.style.display = 'flex'
+    }
+  }
+
+  onTrackClick (event) {
+    const instrument = event.currentTarget.getAttribute('data-instrument')
+    this.instruments[instrument].view.show()
+    this.hide()
   }
 
   destroy() {
-    this.drums && this.drums.view.destroy()
-    this.drums.view = null
-    this.drums.container = null
-    this.drums = null
+    this.removeEventListeners()
 
-    for (let i of this.instruments) {
-      i.view.destroy()
-      i.view = null
-      i.container = null
-      i = null
+    for (let i in this.instruments) {
+      this.instruments[i].view.destroy()
+      this.instruments[i].view = null
+      this.instruments[i].container = null
+      this.instruments[i] = null
     }
 
     this.instruments = null
